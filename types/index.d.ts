726a6b415f53dc9b27ad3b81290b856255d8760e
manuelnelson <elnels@gmail.com>
2020-09-01 13:47:24 -0400

    update typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,3 @@
-import { MIDISender } from "../www/midisender";
-
-// import { MIDISender } from "../www/midisender";
 declare namespace MIDISender {
   function greet() : void
   function sendProgramChange(channelNum:number, programNum:number) : void
@@ -8,9 +5,6 @@ declare namespace MIDISender {
   function sendControlChange(channelNum:number, programNum:number, valueNum:number) : void
   function connectMidi() : void
   function getIncoming(callback:(data:any) => void) : void
-  // interface MIDISender {
-  // }
-  
 }
 
 
