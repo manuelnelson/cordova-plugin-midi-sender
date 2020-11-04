4c93c9d210b465c8d03b102af2b4bf95731d5b2c
Kirsten Bondalapati <bondakir@MacBook-Pro-4.local>
2020-11-03 19:54:35 -0500

    latest

--- src/ios/MIDISender.m
+++ src/ios/MIDISender.m
@@ -24,6 +24,7 @@ NSString* receiveCallbackId;
 @interface MIDISender()
     -(void)sendProgramChange:(CDVInvokedUrlCommand *)command;
     -(void)getIncoming:(CDVInvokedUrlCommand *)command;
+    @property (nonatomic, retain) NSTimer *rescanTimer;
 @end
 
 @implementation MIDISender
@@ -252,48 +253,57 @@ NSString* receiveCallbackId;
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         }];
     }
+    - (void)scanExistingDevices:(NSTimer *)timer
+    {
+        CDVInvokedUrlCommand *command = [timer userInfo]   // @debug
+        NSLog(@"MIDISender:getIncoming was called");
 
+        // create the input port
+        //[self handleIncoming:list procRef:(void *)procRef srcRef:(void *)srcRef]
+        OSStatus s = MIDIInputPortCreate(client, CFSTR("MIDISender Input Port"), midiReceive, (__bridge void *)(self), &inputPort);
+
+        // @debug
+        NSLog(@"MIDISender:getIncoming: Creating MIDI input port (errCode=%d)", (int)s);
+        
+        // attach to all devices for input
+        ItemCount DeviceCount = MIDIGetNumberOfDevices();
+        
+        // @debug
+        NSLog(@"MIDISender:getIncoming: %lu MIDI devices found", DeviceCount);
+
+        for(ItemCount i = 0; i < DeviceCount; i++)
+        {
+            MIDIEndpointRef src = MIDIGetSource(i);
+            MIDIPortConnectSource(inputPort, src, NULL);
+        }
+        
+        if(receiveCallbackId == nil)
+        {
+            receiveCallbackId = command.callbackId;
+            
+            // @debug
+            NSLog(@"MIDISender:getIncoming: receiveCallbackId has been set to %@", receiveCallbackId);
+            
+            //CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString: @"Initialized"];
+        
+            //[pluginResult setKeepCallbackAsBool:YES];
+
+            //[self.commandDelegate sendPluginResult:pluginResult callbackId:receiveCallbackId];
+        }
+    }
     - (void)getIncoming:(CDVInvokedUrlCommand *)command
     {
         // run as background thread
         [self.commandDelegate runInBackground:^{
-
-            // @debug
-            NSLog(@"MIDISender:getIncoming was called");
-
-            // create the input port
-            //[self handleIncoming:list procRef:(void *)procRef srcRef:(void *)srcRef]
-            OSStatus s = MIDIInputPortCreate(client, CFSTR("MIDISender Input Port"), midiReceive, (__bridge void *)(self), &inputPort);
-
-            // @debug
-            NSLog(@"MIDISender:getIncoming: Creating MIDI input port (errCode=%d)", (int)s);
+            [self scanExistingDevices command];
+        
+            self.rescanTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(scanExistingDevices) userInfo:command repeats:YES];
             
-            // attach to all devices for input
-            ItemCount DeviceCount = MIDIGetNumberOfDevices();
-            
-            // @debug
-            NSLog(@"MIDISender:getIncoming: %lu MIDI devices found", DeviceCount);
-
-            for(ItemCount i = 0; i < DeviceCount; i++)
-            {
-                MIDIEndpointRef src = MIDIGetSource(i);
-                MIDIPortConnectSource(inputPort, src, NULL);
-            }
-            
-            if(receiveCallbackId == nil)
-            {
-                receiveCallbackId = command.callbackId;
-                
-                // @debug
-                NSLog(@"MIDISender:getIncoming: receiveCallbackId has been set to %@", receiveCallbackId);
-                
-                CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString: @"Initialized"];
-            
-                [pluginResult setKeepCallbackAsBool:YES];
-
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:receiveCallbackId];
-            }
         }];
     }
-
+    - (void) dealloc
+    {   
+        [_rescanTimer invalidate];
+        self.rescanTimer = nil;
+    }
 @end
