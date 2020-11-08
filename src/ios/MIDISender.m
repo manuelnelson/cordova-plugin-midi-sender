a484aba25712276dd0cc5b6d81bc2706ade8a19a
Kirsten Bondalapati <bondakir@MacBook-Pro-4.local>
2020-11-08 14:09:12 -0500

    latest

--- src/ios/MIDISender.m
+++ src/ios/MIDISender.m
@@ -296,7 +296,7 @@ NSString* receiveCallbackId;
     {
         // run as background thread'
         // [self.commandDelegate evalJs:@"console.log('foo')"];
-        self.rescanTimer = [NSTimer  scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(scanExistingDevices) userInfo:nil repeats:YES];
+        self.rescanTimer = [NSTimer  scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(scanExistingDevices:) userInfo:nil repeats:YES];
 
 
         // [self.commandDelegate runInBackground:^{
